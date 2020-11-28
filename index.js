f5cf7d93bff1450b82826016ccd1a6903fb79a23
Luis Nesi <lenesi@hotmail.com>
2020-11-28 09:03:10 +0000

    parse input and data structure wip

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
 const { parse } = require("path");
 const { exit } = require("process");
 var input = null;
@@ -14,6 +16,7 @@ try {
 class Planet {
   constructor(sizeX, sizeY) {
     this.grid = [];
+    this.robots = [];
     for (var i = 0; i < sizeX; i++) {
       this.grid.push([]);
       for (var j = 0; j < sizeY; j++) {
@@ -21,13 +24,28 @@ class Planet {
       }
     }
   }
+  addRobot(robot) {
+    this.robots.push(robot);
+  }
 }
 
-class Robot {}
+class Robot {
+  constructor(iniPos = ["0", "0", "N"], instructions) {
+    this.position = [parseInt(iniPos[0]), parseInt(iniPos[1])];
+    this.orientation = iniPos[2];
+    this.uuid = uuidv4();
+  }
+}
 
 let coordinates = input[0].split(" ");
 coordinates = coordinates.map((v) => parseInt(v));
 const mars = new Planet(coordinates[0], coordinates[1]);
+input.splice(0, 1);
+
+const robotCount = input.length / 2;
+
+for (var i = 0; i < input.length; i += 2) {
+  mars.addRobot(new Robot(input[i].split(" "), input[i + 1]));
+}
+
 console.log(mars);
-const robotCount = (input.length - 1) / 2;
-console.log();
