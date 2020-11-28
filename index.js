a7344391d9bf7b101823fa5637962dc3bff27f1b
Luis Nesi <lenesi@hotmail.com>
2020-11-28 09:32:01 +0000

    rotation

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var input = null;
 try {
   var data = fs.readFileSync("input.txt", "utf8");
   input = data.toString().split("\n");
-  console.log(input);
 } catch (e) {
   console.log("Invalid Input");
   exit();
@@ -26,14 +25,77 @@ class Planet {
   }
   addRobot(robot) {
     this.robots.push(robot);
+    robot.planet = this;
   }
 }
 
 class Robot {
   constructor(iniPos = ["0", "0", "N"], instructions) {
+    this.planet = null;
     this.position = [parseInt(iniPos[0]), parseInt(iniPos[1])];
     this.orientation = iniPos[2];
-    this.uuid = uuidv4();
+    this.uuid = this.uuid = uuidv4();
+    this.instructions = instructions;
+  }
+
+  followInstructions() {
+    for (var i = 0; i < this.instructions.lengthl; i++) {
+      switch (this.instructions[i]) {
+        case "L":
+          this.rotate("L");
+          break;
+        case "R":
+          this.rotate("R");
+          break;
+        case "F":
+          switch (this.orientation) {
+            case "N":
+              break;
+            case "S":
+
+            case "E":
+
+            case "W":
+          }
+          break;
+      }
+    }
+  }
+  rotate(direction) {
+    switch (direction) {
+      case "L":
+        switch (this.orientation) {
+          case "N":
+            this.orientation = "E";
+            break;
+          case "S":
+            this.orientation = "W";
+            break;
+          case "E":
+            this.orientation = "S";
+            break;
+          case "W":
+            this.orientation = "N";
+            break;
+        }
+        break;
+      case "R":
+        switch (this.orientation) {
+          case "N":
+            this.orientation = "W";
+            break;
+          case "S":
+            this.orientation = "E";
+            break;
+          case "E":
+            this.orientation = "N";
+            break;
+          case "W":
+            this.orientation = "S";
+            break;
+        }
+        break;
+    }
   }
 }
 
@@ -41,11 +103,14 @@ let coordinates = input[0].split(" ");
 coordinates = coordinates.map((v) => parseInt(v));
 const mars = new Planet(coordinates[0], coordinates[1]);
 input.splice(0, 1);
-
 const robotCount = input.length / 2;
 
 for (var i = 0; i < input.length; i += 2) {
-  mars.addRobot(new Robot(input[i].split(" "), input[i + 1]));
+  mars.addRobot(new Robot(input[i].split(" "), Array.from(input[i + 1])));
 }
 
 console.log(mars);
+
+for (var i = 0; i < robotCount; i++) {
+  mars.robots[i].followInstructions();
+}
