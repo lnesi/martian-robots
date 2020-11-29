bd6a4f07b9309aea1bb2912fdd33eec84f728c48
Luis Nesi <lenesi@hotmail.com>
2020-11-29 07:48:07 +0000

    fix all program and finish

--- index.js
+++ index.js
@@ -14,11 +14,13 @@ try {
 
 class Planet {
   constructor(sizeX, sizeY) {
+    this.sizeX = sizeX;
+    this.sizeY = sizeY;
     this.grid = [];
     this.robots = [];
-    for (var i = 0; i < sizeX; i++) {
+    for (var i = 0; i <= sizeX; i++) {
       this.grid.push([]);
-      for (var j = 0; j < sizeY; j++) {
+      for (var j = 0; j <= sizeY; j++) {
         this.grid[i][j] = 0;
       }
     }
@@ -40,8 +42,6 @@ class Robot {
   }
 
   followInstructions() {
-    //console.log(this.instructions, this.position, this.orientation);
-
     for (var i = 0; i < this.instructions.length; i++) {
       switch (this.instructions[i]) {
         case "L":
@@ -49,69 +49,92 @@ class Robot {
           this.rotate(this.instructions[i]);
           break;
         case "F":
-          if (this.canMove(this.orientation)) {
+          if (this.canMove()) {
+            var fromX = this.position.x;
+            var fromY = this.position.y;
             switch (this.orientation) {
               case "N":
-                this.position.y += 1;
+                this.position.y++;
                 break;
               case "S":
-                this.position.y -= 1;
+                this.position.y--;
+
                 break;
               case "E":
-                this.position.x += 1;
+                this.position.x++;
+
                 break;
               case "W":
-                this.position.x -= 1;
+                this.position.x--;
                 break;
             }
+            this.isLost(fromX, fromY);
           }
           break;
       }
-      this.isLost();
     }
-    console.log(this);
   }
   canMove() {
-    return true;
+    if (this.lost) return false;
+    if (!this.planet.grid[this.position.x][this.position.y]) {
+      // no spect
+      return true;
+    } else {
+      //yes spect
+      if (this.position.x === 0 && this.orientation === "W") return false; // no w
+      if (this.position.x === this.planet.sizeX && this.orientation === "E")
+        return false; //no
+      if (this.position.y === 0 && this.orientation === "S") return false; // no S
+      if (this.position.y === this.planet.sizeY && this.orientation === "N")
+        return false; //no N
+      return true;
+    }
   }
-  isLost() {
-    this.lost =
+  isLost(fromX, fromY) {
+    if (
       this.position.x >= this.planet.grid.length ||
       this.position.x < 0 ||
       this.position.y < 0 ||
-      this.position.y >= this.planet.grid[0].length;
+      this.position.y >= this.planet.grid[0].length
+    ) {
+      this.lost = true;
+      this.planet.grid[fromX][fromY] = this.uuid;
+      // set last known position
+      this.position.x = fromX;
+      this.position.y = fromY;
+    }
   }
   rotate(direction) {
     switch (direction) {
       case "L":
         switch (this.orientation) {
           case "N":
-            this.orientation = "E";
-            break;
-          case "S":
             this.orientation = "W";
             break;
+          case "S":
+            this.orientation = "E";
+            break;
           case "E":
-            this.orientation = "S";
+            this.orientation = "N";
             break;
           case "W":
-            this.orientation = "N";
+            this.orientation = "S";
             break;
         }
         break;
       case "R":
         switch (this.orientation) {
           case "N":
-            this.orientation = "W";
-            break;
-          case "S":
             this.orientation = "E";
             break;
+          case "S":
+            this.orientation = "W";
+            break;
           case "E":
-            this.orientation = "N";
+            this.orientation = "S";
             break;
           case "W":
-            this.orientation = "S";
+            this.orientation = "N";
             break;
         }
         break;
@@ -129,8 +152,15 @@ for (var i = 0; i < input.length; i += 2) {
   mars.addRobot(new Robot(input[i].split(" "), Array.from(input[i + 1])));
 }
 
-//console.log(mars);
-
 for (var i = 0; i < robotCount; i++) {
   mars.robots[i].followInstructions();
+  console.log(
+    mars.robots[i].position.x +
+      " " +
+      mars.robots[i].position.y +
+      " " +
+      mars.robots[i].orientation +
+      " " +
+      (mars.robots[i].lost ? "LOST" : "")
+  );
 }
